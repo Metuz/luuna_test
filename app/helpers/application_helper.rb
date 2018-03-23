05b6af4bf87b9e459ae7f591908c5fcc9049c94e
Alejandro Hernández <alejandroohr@gmail.com>
2018-03-22 22:19:07 -0600

    Adding Welcome Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def login_helper
+    if user_signed_in?
+      link_to 'Logout', destroy_user_session_path, method: :delete
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get 'pages/welcome'
+
   devise_for :users,
              path: '/',
              path_names: { sign_in: 'login', sign_out: 'logout' },
              skip: [:confirmation, :unlock, :registration, :sign_up, :password]
   resources :users
   resources :addresses
+
+  devise_scope :user do
+    authenticated :user do
+      root to: 'pages#welcome', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
